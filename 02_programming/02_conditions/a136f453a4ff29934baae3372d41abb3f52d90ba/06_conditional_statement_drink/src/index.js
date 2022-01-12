3f06c668ff9d6d35540fa7062aea8122ce90c63e
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-12 11:53:40 +0100

    Exercice conditions 06 clear

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/06_conditional_statement_drink/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/06_conditional_statement_drink/src/index.js
@@ -2,8 +2,20 @@
 
 // Create two objects, `frieda`, `francis` with the fields :
 //   `age` (an integer) and `name`, (obviously a string)
+const frieda = {
+  age : 22,
+  name : "Frieda"
+};
 
-
-
+const francis = {
+  age : 17,
+  name : "Francis"
+};
 // Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer,
 // respectively, to the question "Can he / she drink alcohol?".
+
+let canFriedaDrinkAlcohol;
+let canFrancisDrinkAlcohol;
+
+frieda.age>=18 ? canFriedaDrinkAlcohol=true : canFriedaDrinkAlcohol=false;
+francis.age>=18 ? canFrancisDrinkAlcohol=true : canFrancisDrinkAlcohol=false;
