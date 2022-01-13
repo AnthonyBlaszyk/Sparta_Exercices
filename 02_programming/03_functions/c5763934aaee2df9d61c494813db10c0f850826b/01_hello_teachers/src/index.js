3dc72fa4f87d22e4a82438e7eab7ee7234061ef7
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-13 11:21:45 +0100

    Exercice functions 01 clear

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/01_hello_teachers/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/01_hello_teachers/src/index.js
@@ -1,6 +1,10 @@
 function sayHelloToTeachers(teachers) {
   // Code the function here.
-
+  console.log(`Hello ${teachers[0]}!`);
+  console.log(`Hello ${teachers[1]}!`);
+  console.log(`Hello ${teachers[2]}!`);
+  console.log(`Hello ${teachers[3]}!`);
+  console.log(`Hello ${teachers[4]}!`);
 }
 
 // Do not remove last lines, it is for tests
