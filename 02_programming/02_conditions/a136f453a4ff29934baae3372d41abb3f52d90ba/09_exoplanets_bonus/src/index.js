a52db35dc46ff65e662f803027340c9c1688be4c
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-13 09:00:11 +0100

    Exercice conditions 09

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/09_exoplanets_bonus/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/09_exoplanets_bonus/src/index.js
@@ -1,6 +1,7 @@
 // Don't modify these lines for tests to work properly.
 const star = require("./data/starData");
 const planet = require("./data/exoplanetData");
+const { spectralClass } = require("./data/starData");
 
 // Your code here:
 let canHabitateLife;
