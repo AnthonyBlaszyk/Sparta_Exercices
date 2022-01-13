cf3d221c72e9638b6ac04a6f1b0ddb7a005501e2
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-13 12:20:06 +0100

    Exercice functions 03 clear

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
@@ -1,5 +1,10 @@
-function oddEven() {
+function oddEven(number) {
   // Code the function here.
+  if(Number.isInteger(number)){
+    return number%2 === 0 ? `${number} is an even number.` : `${number} is an odd number.`;
+  }else{
+    return console.log(`${number} is not a number.`);
+  }
 }
 
 // Do not remove last lines, it is for tests
