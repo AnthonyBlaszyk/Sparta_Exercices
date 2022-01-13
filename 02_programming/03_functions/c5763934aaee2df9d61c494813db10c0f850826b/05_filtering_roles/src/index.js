f90a299e4d69619128abdc88b2b8b801dd71022d
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-13 14:16:21 +0100

    Exercice function 05 clear

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,7 +1,20 @@
-function filteredRoles(){
+function filteredRoles(personArray, roles = "Teacher"){
   // Code the function here.
+  const mySortedArray = [];
 
+  if(personArray[0].role === roles){
+    mySortedArray.push({name:personArray[0].name, role:roles});
+  }
+  if(personArray[1].role === roles){
+    mySortedArray.push({name:personArray[1].name, role:roles});
+  }
+  if(personArray[2].role === roles){
+    mySortedArray.push({name:personArray[2].name, role:roles});
+  }
+  if(personArray[3].role === roles){
+    mySortedArray.push({name:personArray[3].name, role:roles});
+  }
+  return mySortedArray;
 }
-
 // Do not remove lines below, it is for tests
 module.exports = filteredRoles;
