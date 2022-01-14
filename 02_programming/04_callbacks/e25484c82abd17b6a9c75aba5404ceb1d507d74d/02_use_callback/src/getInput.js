fd8dbc1f309289aa2d4d55e92ad1592299b7f021
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-14 10:43:47 +0100

    Exercice callback 02 clear

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -1,5 +1,9 @@
 // A function getInput that takes two parameters, an input (the first one) and a callback function
 
-const getInput = // code the function here
-  // Leave the line below for tests to work
-  (module.exports = getInput);
+// code the function here
+const getInput = (userImput, callbackFunction) => {
+  callbackFunction(userImput.key);
+};
+
+// Leave the line below for tests to work
+module.exports = getInput;
