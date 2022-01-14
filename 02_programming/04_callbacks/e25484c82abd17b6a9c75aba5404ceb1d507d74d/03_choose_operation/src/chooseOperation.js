80ef66c0595625a1dbd21a25c3780fd7c04cdb16
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-14 10:55:26 +0100

    Exercice callback 03 clear

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,21 +1,30 @@
-function addition() {
+function addition(numberOne, numberTwo) {
   // Code here
+  return numberOne+numberTwo;
 }
 
-function substraction() {
+function substraction(numberOne, numberTwo) {
   // Code here
+  return numberOne-numberTwo;
 }
 
-function multiplication() {
+function multiplication(numberOne, numberTwo) {
   // Code here
+  return numberOne*numberTwo;
 }
 
-function division() {
+function division(numberOne, numberTwo) {
   // Code here
+  return numberOne/numberTwo;
 }
 
-function chooseOperation() {
+function chooseOperation(numberOne, numberTwo, operation) {
   // Code here
+  if(Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
+    return operation(numberOne, numberTwo);
+  }else{
+    throw new Error("Parameters are not integer");
+  }
 }
 
 // Leave the line below for tests to work
