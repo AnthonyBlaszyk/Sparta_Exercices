abea14ec04e27597ba85912d91060ede5153bf1f
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-17 10:18:18 +0100

    Exercice iterators 01 clear

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
@@ -1,7 +1,10 @@
 function greeter(people) {
   // Code the function here.
-
+  people.forEach((people)=>{
+    console.log(`Hello ${people}!`);
+  });
 }
+
 
 // Do not remove the following lines, it is for tests
 module.exports = greeter;
