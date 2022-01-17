8ef1eea53fc866b2dbca851f54a32c3db6811def
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-17 13:18:41 +0100

    Exercice iterators 06 clear

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/06_how_much_did_i_make/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/06_how_much_did_i_make/src/index.js
@@ -1,12 +1,29 @@
 function howMuchDidIMake(invoices) {
   // Code here
+  const tipArray = [];
+
+  invoices.forEach((invoice)=>{
+    if(invoice.waiter === "Yourself"){
+      tipArray.push(invoice.tip);
+    }
+  });
+
+  const totalTip = tipArray.reduce((tip, base)=>{
+    return tip + base;
+  });
+
+  return totalTip;
 }
 
 const invoices = [
   { clients: 3, waiter: "Yourself", price: 5100, tip: 200 },
   { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
   { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
+  { clients: 2, waiter: "Nobody", price: 2900, tip: 300 },
+  { clients: 2, waiter: "Yourself", price: 2900, tip: 700 },
 ];
+
+howMuchDidIMake(invoices);
 
 // Do not remove the following line, it is for tests
 module.exports = howMuchDidIMake;
