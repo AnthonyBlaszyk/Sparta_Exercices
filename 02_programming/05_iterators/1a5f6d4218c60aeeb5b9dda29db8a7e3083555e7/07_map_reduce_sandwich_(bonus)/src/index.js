c78da7e638e6fea8bcb1ba53bba171cf33a0aafe
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-17 16:19:47 +0100

    Exercice iterators 07 bonus clear

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/07_map_reduce_sandwich_(bonus)/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/07_map_reduce_sandwich_(bonus)/src/index.js
@@ -1,10 +1,31 @@
 function slice(ingredients) {
   // Code here
+  return ingredients.map((ingredient) => {
+    return "sliced " + ingredient;
+  });
 }
 
 function makeSandwich(ingredients) {
   // Code here
+  if (
+    ingredients.every((ingredient) => {
+      return ingredient.includes("sliced");
+    })
+
+  ) {
+    console.log(ingredients);
+    return "A tasty sandwich made of" + ingredients.map((ingredient) => {
+      return ingredient.slice(6);
+    });
+
+  } else {
+    return "I can't make a sandwich with non-sliced ingredients";
+  }
 }
+
+// console.log(slice(["ham", "lettuce", "tomato"]));
+// console.log(makeSandwich(["sliced ham", "sliced lettuce", "sliced tomato"]));
+// console.log(makeSandwich(["sliced ham", "sliced lettuce", "tomato"]));
 
 // Do not remove the following line, it is for tests
 module.exports = {
