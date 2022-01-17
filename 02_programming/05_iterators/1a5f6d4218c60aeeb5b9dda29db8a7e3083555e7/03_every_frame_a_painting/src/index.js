ac874b34b75429c04ef1cd96e5771603a4416dfd
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-17 11:36:38 +0100

    Exercice iterators 03 clear

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
@@ -1,6 +1,22 @@
 function canTakeArtPieces(pieces) {
-
+  const filteredArray = [];
+  pieces.forEach((piece)=>{
+    filteredArray.push(piece.type);
+  });
+  return filteredArray.every((type)=>{
+    return type === "painting";
+  });
 }
+
+const test =
+  [
+    {piece: "The Thinker", type: "statue"},
+    {piece: "Mona Lisa", type: "painting"},
+    {piece: "Guernica", type: "painting"}
+  ];
+
+
+canTakeArtPieces(test);
 
 // Do not remove the following line, it is for tests
 module.exports = canTakeArtPieces;
