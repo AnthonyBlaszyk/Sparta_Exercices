eec6f00b288c9761677c5f36aa93120bdf09b031
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-17 11:49:08 +0100

    Exercice iterators 05 clear

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
@@ -1,5 +1,13 @@
 function onlyMyTips(invoices) {
   // Code here
+  const tipArray = [];
+  invoices.forEach((invoice)=>{
+    if(invoice.waiter === "Yourself"){
+      tipArray.push(invoice.tip);
+    }
+  });
+  console.log(tipArray);
+  return tipArray;
 }
 
 const todaysInvoices = [
@@ -7,6 +15,8 @@ const todaysInvoices = [
   { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
   { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
 ];
+
+onlyMyTips(todaysInvoices);
 
 // Do not remove the following line, it is for tests
 module.exports = onlyMyTips;
