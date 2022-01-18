08d6cdce29deabe4eeef7cc79bf949a1fa74700c
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 17:16:25 +0100

    Exercice loops 09 bonus WIP

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/09_colorful_numbers_bonus/src/colorful.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/09_colorful_numbers_bonus/src/colorful.js
@@ -2,5 +2,10 @@ function isColorful(number) {
   // Code here
 }
 
+console.log(isColorful(3));
+console.log(isColorful(32));
+console.log(isColorful(324));
+console.log(isColorful(3245));
+
 // Do not remove last lines, it is for tests
 module.exports = isColorful;
