bbc7abe184d4148cac89db55b14856734311cfbc
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 10:11:04 +0100

    Exercice loops 03 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
@@ -1,7 +1,21 @@
-function mapLoop() {
+function mapLoop(array, map) {
   // Code the function here.
-
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    resultArray.push(map(array[i]));
+  }
+  return resultArray;
 }
+
+const numbers = [1, 2, 3, 4];
+
+function double(number) {
+  return number * 2;
+}
+
+const doubledNumbers = mapLoop(numbers, double);
+
+console.log(doubledNumbers);
 
 // Do not remove the following lines, it is for tests
 module.exports = mapLoop;
