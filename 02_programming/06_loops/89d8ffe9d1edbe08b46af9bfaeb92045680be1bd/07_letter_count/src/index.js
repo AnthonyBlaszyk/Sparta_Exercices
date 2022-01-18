795e96d6e3b1c931e473cbb07d37e5bab70f4681
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 11:07:35 +0100

    Exercice loops 07 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
@@ -1,6 +1,14 @@
-function letterCount() {
+function letterCount(string, chara) {
   // Code here
+  let numberOfChara = 0;
+  for(let i=0; i<string.length; i++){
+    if(chara === string[i]){
+      numberOfChara++;
+    }
+  }
+  return numberOfChara;
 }
+
 
 // Do not remove the following line, it is for tests
 module.exports = letterCount;
