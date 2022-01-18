35e8f69c6643ad3d317cd7a2000d983b6edb1f00
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 10:04:50 +0100

    Exercice loops 02 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
@@ -1,6 +1,8 @@
-function forEachLoop() {
+function forEachLoop(array, funct) {
   // Code the function here.
-
+  for (let i = 0; i < array.length; i++) {
+    funct(array[i]);
+  }
 }
 
 // Do not remove the following lines, it is for tests
