45b5709fa7229d5fa869f80627fbc29b8fe729ee
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 10:28:51 +0100

    Exercice loops 04 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
@@ -1,6 +1,20 @@
-function sing () {
+function sing() {
   // code here
-};
+  let lirycs = [];
+  for (let i = 99; i > 0; i--) {
+    if (i > 2) {
+      lirycs.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`, `Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
+    } else if(i > 1){
+      lirycs.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`, `Take one down and pass it around, ${i - 1} bottle of beer on the wall.`);
+    }
+    else {
+      lirycs.push(`${i} bottle of beer on the wall, ${i} bottle of beer.`, "Take one down and pass it around, no more bottles of beer on the wall.");
+    }
+  }
+  lirycs.push("No more bottles of beer on the wall, no more bottles of beer.");
+  lirycs.push("Go to the store and buy some more, 99 bottles of beer on the wall.");
+  return lirycs;
+}
 
 // Do not remove the following line, it is for tests
 module.exports = sing;
