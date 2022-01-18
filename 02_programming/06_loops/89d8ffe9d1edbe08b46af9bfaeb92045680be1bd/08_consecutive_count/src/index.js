11883799082099ee0d64ef30ce496ac087950af1
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 11:31:10 +0100

    Exercice loops 08 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
@@ -1,6 +1,25 @@
-function consecutiveCount() {
+function consecutiveCount(word, chara) {
   // Code here
+  let charaCount = 0;
+  let tmpCount = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === chara && word[i] !== word[i - 1]) {
+      tmpCount = 1;
+      if (tmpCount > charaCount) {
+        charaCount = tmpCount;
+      }
+    } else if (word[i] === chara && word[i] === word[i - 1]) {
+      tmpCount++;
+      if (tmpCount > charaCount) {
+        charaCount = tmpCount;
+      }
+    }
+  }
+  return charaCount;
 }
+
+console.log(consecutiveCount("aaaabcaaabaa", "a"));
 
 // Do not remove the following line, it is for tests
 module.exports = consecutiveCount;
