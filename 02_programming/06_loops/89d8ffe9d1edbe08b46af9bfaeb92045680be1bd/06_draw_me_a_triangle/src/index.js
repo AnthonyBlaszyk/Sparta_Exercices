905ece13c91c294a68c4d37591b4994de3b3ffc8
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 11:01:04 +0100

    Exercice loops 06 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
@@ -1,5 +1,10 @@
-function triangle() {
+function triangle(triangleSize) {
   // Code here
+  let triangleLine = "";
+  for (let i = 0; i < triangleSize; i++) {
+    triangleLine += "*";
+    console.log(triangleLine);
+  }
 }
 
 // Do not remove the following line, it is for tests
