f0485bb9fa6987e1c1fd5101ad9ab8b54e697259
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-18 09:59:50 +0100

    Exercice loops 01 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/01_greeter_for/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/01_greeter_for/src/index.js
@@ -1,6 +1,8 @@
 function greeter(people) {
   // Code the function here.
-
+  for (let i = 0; i < people.length; i++) {
+    console.log(`Hello ${people[i]}!`);
+  }
 }
 
 // Do not remove the following lines, it is for tests
