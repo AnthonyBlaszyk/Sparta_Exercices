0d32dbb52b8d7cdc852c9a2e5aca61ee25790184
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-19 10:10:01 +0100

    Exercice firstProgram 01 clear

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/01_are_you_ready/src/readyForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/01_are_you_ready/src/readyForm.js
@@ -1,5 +1,10 @@
 const readyForm = (reader) => {
   // Code here
+  console.log("Hello user!");
+  reader.question("Are you ready for today ??!\n", (ready) => {
+    console.log(`WOW, ${ready} ??!! Really Nice! Let's goo !!.`);
+    reader.close();
+  });
 };
 
 // Leave line below for tests to work
