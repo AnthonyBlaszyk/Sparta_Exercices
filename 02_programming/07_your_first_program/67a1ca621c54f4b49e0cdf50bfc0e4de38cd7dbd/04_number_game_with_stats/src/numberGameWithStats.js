c18aed3de03b92eb9284de9b83581f1b657cd7a4
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-19 12:10:15 +0100

    Exercice firstProgram 04 clear

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
@@ -1,5 +1,47 @@
 function numberGameWithStats(reader, min = 1, max = 100) {
   // Code here
+  const randomNumber = getRandomNumber(min, max);
+  let counter = 0;
+  console.log(randomNumber);
+
+  function question() {
+    reader.question("Enter a number\n", (number) => {
+      const userNumber = parseInt(number);
+
+      if (!Number.isInteger(userNumber)) {
+        console.log("It's not a number");
+        counter++;
+        question();
+      } else if (userNumber > max || userNumber < min) {
+        console.log(`The number is between ${min} and ${max}`);
+        counter++;
+        question();
+      } else if (userNumber > randomNumber) {
+        console.log("Too high");
+        counter++;
+        question();
+      } else if (userNumber < randomNumber) {
+        console.log("Too low");
+        counter++;
+        question();
+      } else {
+        console.log("You won");
+        counter++;
+        if (counter > 1) {
+          console.log(`you suceed after ${counter} try`);
+        } else {
+          console.log(`Wow, you succed after only ${counter} try !!`);
+        }
+        reader.close();
+      }
+    });
+  }
+
+  question();
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
 }
 
 module.exports = numberGameWithStats;
