46c3ab56cba3b3870e814f257a4cd06bc1b1240e
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-19 11:40:19 +0100

    Exercice firstProgram 03 clear

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
@@ -1,5 +1,41 @@
 function numberGame(reader, min = 1, max = 100) {
   // code here
+  const randomNumber = getRandomNumber(min, max);
+
+  console.log(randomNumber);
+
+  console.log("Welcome!");
+  console.log("You have to find the right number, between 1 and 100!");
+  console.log("Good luck!!");
+
+  function question() {
+    reader.question("Enter a number\n", (number) => {
+      let userNumber = parseInt(number);
+
+      if (!Number.isInteger(userNumber)) {
+        console.log("This was not a number");
+        question();
+      } else if (userNumber > max || userNumber < min) {
+        console.log(`The number is between ${min} and ${max}`);
+        question();
+      } else if (userNumber > randomNumber) {
+        console.log("Too high");
+        question();
+      } else if (userNumber < randomNumber) {
+        console.log("Too low");
+        question();
+      } else if (userNumber === randomNumber) {
+        console.log("you won");
+        reader.close();
+      }
+    });
+  }
+
+  question();
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
 }
 
 module.exports = numberGame;
