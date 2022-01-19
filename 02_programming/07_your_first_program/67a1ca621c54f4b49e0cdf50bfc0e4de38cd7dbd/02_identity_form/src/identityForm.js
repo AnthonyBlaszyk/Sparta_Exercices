7e8c8c34ee61fe2a9220503441b12a86757f5ccf
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-19 10:15:49 +0100

    Exercice firstProgram 02 clear

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,5 +1,13 @@
 const identityForm = (reader) => {
   // Code here
+  reader.question("first name\n", (firstname) => {
+    reader.question("last name ?\n", (lastname) => {
+      reader.question("age ?\n", (age) => {
+        console.log(`Your name is ${firstname} ${lastname} and you are ${age}.`);
+        reader.close();
+      });
+    });
+  });
 };
 
 // Leave line below for tests to work
