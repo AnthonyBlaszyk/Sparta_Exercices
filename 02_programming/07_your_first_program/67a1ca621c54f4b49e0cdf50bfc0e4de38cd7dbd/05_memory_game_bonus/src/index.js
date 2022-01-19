19481b8a7636317f283a3ff4cf21ea94d945e240
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-19 17:46:54 +0100

    Exercice firstProgram 05 bonus WIP

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/05_memory_game_bonus/src/index.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/05_memory_game_bonus/src/index.js
@@ -1,6 +1,11 @@
 const readline = require("readline");
 const clear = require("./clear");
 
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const symbols = {
   tree: "🌳",
   ghost: "👻",
@@ -23,3 +28,39 @@ const symbols = {
   salad: "🥗",
   clown: "🤡",
 };
+
+const arrayOfSymbols = Object.entries(symbols);
+
+function getBoardSize() {
+  reader.question("Choose an even board size between 4 and 40\n", (number) => {
+    let userNumber = parseInt(number);
+    if (!Number.isInteger(userNumber)) {
+      console.log("that's not a number");
+      getBoardSize();
+    } else if (userNumber % 2 !== 0) {
+      console.log("that's an odd number, please choose an even number");
+      getBoardSize();
+    } else if (userNumber < 4 || userNumber > 40) {
+      console.log("please choose a number between 4 and 40");
+      getBoardSize();
+    } else {
+      reader.close();
+      return parseInt(userNumber);
+    }
+  });
+}
+
+function generateBoard(boardSize) {
+  const board = [];
+  for (let i = 0; i < boardSize / 2; i++) {
+    board.push({ i: arrayOfSymbols[i][1] });
+    board.push({ i: arrayOfSymbols[i][1] });
+  }
+  console.log(board);
+  console.log(Object.keys(board));
+  return board;
+}
+
+const boardGame = generateBoard(6);
+
+(x) => x + 5;
