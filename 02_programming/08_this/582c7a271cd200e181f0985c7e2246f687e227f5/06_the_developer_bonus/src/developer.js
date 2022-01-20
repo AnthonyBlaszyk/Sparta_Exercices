ca38154d240fdc61ec4a17401ccd6823b5aa8dbd
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-20 17:12:02 +0100

    Exercice this 06 bonus clear

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/06_the_developer_bonus/src/developer.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/06_the_developer_bonus/src/developer.js
@@ -1,3 +1,69 @@
-const developer = {};
+const developer = {
+  isCoding: false,
+  daysCoding: 0,
+  projectStatus: "waiting for a project",
+  codeLinesProduced: 0,
+  linesNeeded: 0,
+  maxDelay: 0,
 
-module.exports = developer;
+  initiateProject: function () {
+    this.isCoding = true;
+    this.daysCoding = 0;
+    this.codeLinesProduced = 0;
+    return this;
+  },
+  startProject: function (linesNeeded, maxDelay) {
+    this.linesNeeded = linesNeeded;
+    this.maxDelay = maxDelay;
+    return this;
+  },
+
+  codeForOneDay: function () {
+    const randomBug = this.generateBug();
+    if (this.codeLinesProduced < 80) {
+      if (randomBug > 1) {
+        this.codeLinesProduced += 2;
+        this.daysCoding++;
+      } else {
+        this.codeLinesProduced += 2;
+        this.daysCoding += 3;
+      }
+    } else if (this.codeLinesProduced < 150) {
+      if (randomBug > 2) {
+        this.codeLinesProduced += 2;
+        this.daysCoding++;
+      } else {
+        this.codeLinesProduced += 2;
+        this.daysCoding += 3;
+      }
+    } else {
+      if (randomBug > 4) {
+        this.codeLinesProduced += 2;
+        this.daysCoding++;
+      } else {
+        this.codeLinesProduced += 2;
+        this.daysCoding += 3;
+      }
+    }
+    return this;
+  },
+
+  generateBug: function () {
+    return Math.round(Math.random() * 10);
+  },
+
+  mightStop: function () {
+    if (this.maxDelay - 1 >= this.daysCoding) {
+      this.projectStatus = "project finished";
+      this.isCoding = false;
+    } else if (this.linesNeeded === this.codeLinesProduced) {
+      this.projectStatus = "project stopped";
+      this.isCoding = false;
+    }
+    return this;
+  },
+};
+
+module.exports = {
+  developer,
+};
