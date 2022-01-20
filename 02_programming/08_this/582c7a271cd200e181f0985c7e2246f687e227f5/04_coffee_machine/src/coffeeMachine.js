badefce723f0991519a19cb82284b9fcd78834f1
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-20 10:49:53 +0100

    Exercice this 04 clear

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/04_coffee_machine/src/coffeeMachine.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/04_coffee_machine/src/coffeeMachine.js
@@ -1,5 +1,26 @@
 const machine = {
   // Complete here
+  litersOfCoffee: 0,
+  fillWithLitersOfCoffee: function (litersAdded) {
+    this.litersOfCoffee += litersAdded;
+    return this;
+  },
+  expresso: function () {
+    if (this.litersOfCoffee >= 0.08) {
+      this.litersOfCoffee -= 0.08;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  longCoffee: function () {
+    if (this.litersOfCoffee >= 0.15) {
+      this.litersOfCoffee -= 0.15;
+      return true;
+    } else {
+      return false;
+    }
+  },
 };
 
 module.exports = machine;
