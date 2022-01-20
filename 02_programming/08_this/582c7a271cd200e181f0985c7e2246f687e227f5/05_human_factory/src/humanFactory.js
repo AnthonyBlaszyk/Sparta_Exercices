fb62e1646b4f3baf67631360168531482a7e2d7f
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-20 15:40:00 +0100

    Exercice this 05 clear

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/05_human_factory/src/humanFactory.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/05_human_factory/src/humanFactory.js
@@ -1,10 +1,40 @@
 const humanFactory = (humanData = {}) => {
   // code here
+  let human = {
+    firstName: "John",
+    lastName: "Doe",
+    genre: "male",
+    job: "unemployed",
+    fullname: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    introduction: function () {
+      return `Hello! My name is ${this.fullname()}.`;
+    },
+    ...humanData,
+  };
+
+  if (human.genre === "female" && human.firstName === "John") {
+    human.firstName = "Jane";
+  }
+
+  return human;
 };
 
 const createHumans = (humans) => {
   // code here
+  return humans.map((human) => {
+    return humanFactory(human);
+  });
 };
+
+console.log(createHumans([{ genre: "female" }]));
+
+// const [john, jane] = [createHumans({}), createHuman({ genre: "female" })];
+
+// const [jane, john] = createHumans([{ genre: "female" }, {}]);
+// console.log(jane);
+// console.log(john);
 
 module.exports = {
   humanFactory,
