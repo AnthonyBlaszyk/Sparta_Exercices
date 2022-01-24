6cc51b3d44f21da5856ac70d8d6c0a01ef043d2d
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-24 10:32:58 +0100

    Exercie module 01 clear

--- 02_programming/10_modules/fed8e7548d1850dcfb910812815428243660799b/01_Fundamental_Arrays_into_Objects_warmup/src/arraysIntoObjects.js
+++ 02_programming/10_modules/fed8e7548d1850dcfb910812815428243660799b/01_Fundamental_Arrays_into_Objects_warmup/src/arraysIntoObjects.js
@@ -1,5 +1,10 @@
 const arraysIntoObjects = (keysArray, valuesArray) => {
   /// Code your function here
+  let result = {};
+  keysArray.forEach((key, i) => {
+    result[key] = valuesArray[i];
+  });
+  return result;
 };
 
 // Don't change this line for tests to run properly
