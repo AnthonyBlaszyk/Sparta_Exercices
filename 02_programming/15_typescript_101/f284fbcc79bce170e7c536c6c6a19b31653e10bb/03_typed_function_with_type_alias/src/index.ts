4f95e23ad71376b0720c97ae4d475c93c900ec0e
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-01-31 11:20:19 +0100

    Exercice 03 : typescript_101 clear

--- 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
@@ -1,5 +1,11 @@
-function handleTuple() {
+type MyTuple = [string, number];
+
+function handleTuple(array: MyTuple): void {
   // Code the function here.
+  if (typeof array[0] === "string") {
+    console.log(`${array[0]} is a string`);
+    console.log(`${array[1]} is a number`);
+  }
 }
 
 // Leave the line below for tests to work properly.
