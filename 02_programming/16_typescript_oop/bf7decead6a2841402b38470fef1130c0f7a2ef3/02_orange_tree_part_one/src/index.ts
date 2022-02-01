3f86fff6aa3bbab0fe5a742ce50103d22b785694
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-01 10:21:09 +0100

    Exercice 02 : typescript_poo clear

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -1,4 +1,30 @@
 // Code the class here.
+class Tree {
+  age: number;
+  height: number;
+  alive: boolean = true;
+
+  constructor(age: number) {
+    this.age = age;
+    this.height = this.updateHeight();
+  }
+
+  updateHeight(): number {
+    if (this.age <= 9) {
+      return this.age * 25;
+    } else if (this.age <= 20) {
+      return 9 * 25 + (this.age - 9) * 10;
+    } else {
+      return 9 * 25 + 11 * 10;
+    }
+  }
+
+  seed(): void {
+    this.age = 0;
+    this.height = 0;
+    this.alive = true;
+  }
+}
 
 // Leave the line below for tests to work properly.
 export { Tree };
