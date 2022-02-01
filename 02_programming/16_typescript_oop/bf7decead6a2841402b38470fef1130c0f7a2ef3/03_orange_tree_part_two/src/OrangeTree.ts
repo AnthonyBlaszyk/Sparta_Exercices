37a1b98c7c71907d386162f624e384cd278c23b9
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-01 11:21:01 +0100

    Exercice 03 : typescript_poo clear

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/03_orange_tree_part_two/src/OrangeTree.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/03_orange_tree_part_two/src/OrangeTree.ts
@@ -1,6 +1,49 @@
 import { Tree } from "./Tree";
 
 // Code your class here.
+class OrangeTree extends Tree {
+  oranges: string[] = [];
+
+  growOranges(): void {
+    this.oranges = [];
+
+    if (this.age >= 5 && this.age <= 10) {
+      for (let i = 0; i < 10; i++) {
+        this.oranges.push("🍊");
+      }
+    } else if (this.age > 5 && this.age <= 20) {
+      for (let i = 0; i < 20; i++) {
+        this.oranges.push("🍊");
+      }
+    } else if (this.age > 5 && this.age <= 40) {
+      for (let i = 0; i < 5; i++) {
+        this.oranges.push("🍊");
+      }
+    }
+  }
+
+  pickAnOrange(): void {
+    this.oranges.pop();
+  }
+
+  isAlive(): boolean {
+    const randomNumber = Math.floor(Math.random() * 100);
+    if (this.age >= randomNumber && this.age > 50) {
+      return false;
+    } else return true;
+  }
+
+  ageOneYear(): void {
+    this.age++;
+    this.height = this.updateHeight();
+    this.growOranges();
+    this.alive = this.isAlive();
+  }
+
+  seed(): void {
+    super.seed();
+  }
+}
 
 // Leave the line below for tests to work properly.
 export { OrangeTree };
