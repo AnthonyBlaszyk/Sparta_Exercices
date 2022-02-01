2d62c568823e928f0aab1dd62ac15dd932f7c9e3
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-01 09:46:27 +0100

    Cleaning tree

--- 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/07_holiday_location_finder_bonus/src/data/data.ts
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/07_holiday_location_finder_bonus/src/data/data.ts
@@ -1,9 +1,26 @@
-const continents: any = [
+type Continents = string[];
+type Cities = {
+  [key: string]: string[];
+};
+
+const continents: Continents = [
   // Replace 'any' type and write your data here.
+  "Europe",
+  "North America",
+  "South America",
+  "Asia",
+  "Africa",
+  "Oceania",
 ];
 
-const cities: any = {
+const cities: Cities = {
   // Replace 'any' type and write your data here.
+  europe: ["city", "city", "city"],
+  northAmerica: ["city", "city", "city"],
+  southAmerica: ["city", "city", "city"],
+  asia: ["city", "city", "city"],
+  africa: ["city", "city", "city"],
+  oceania: ["city", "city", "city"],
 };
 
 // Leave the line below for tests to work properly.
