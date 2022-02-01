1734470688916b4f0fe62d655a4ab8d29b17d4fd
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-01 09:57:40 +0100

    Exercice 01 : typescript_poo clear

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/01_the_hero_factory/src/index.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/01_the_hero_factory/src/index.ts
@@ -1,4 +1,18 @@
 // Code the class here.
+class Hero {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  sayHi(): string {
+    return `I'm ${this.name}`;
+  }
+}
+
+const batman = new Hero("batman");
+console.log(batman);
 
 // Leave the line below for tests to work properly.
 export { Hero };
