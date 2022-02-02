e76fe4fc75cfe817cb7b0f7d510fe7e015ee7738
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-02 13:47:13 +0100

    Exercice 03 : typescript_generics clear

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
@@ -1,4 +1,16 @@
+type Human = {
+  name: string;
+  age: number;
+  knowsDev: boolean;
+};
 
+type Dev = Human & {
+  knownLanguage: string;
+};
+
+function becomeADev(object: Human): Dev {
+  return { ...object, knowsDev: true, knownLanguage: "JavaScript" };
+}
 
 // Leave the line below for tests to work properly.
 export { becomeADev };
