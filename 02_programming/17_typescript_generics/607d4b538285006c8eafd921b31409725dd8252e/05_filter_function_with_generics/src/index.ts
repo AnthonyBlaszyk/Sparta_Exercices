b10179ac9d20ee215488f3049766f6617b6b1ff9
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-02 14:24:52 +0100

    Exercice 05 typescript_generics clear

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
@@ -1,13 +1,16 @@
-function filterFor(array, predicate) {
+function filterFor<ArrayElementType>(
+  array: ArrayElementType[],
+  predicate: (element: ArrayElementType) => boolean,
+): ArrayElementType[] {
   const resultArray = [];
 
   for (const element of array) {
     if (predicate(element)) {
-      resultArray.push(element)
+      resultArray.push(element);
     }
   }
 
   return resultArray;
 }
 
-export { filterFor }
+export { filterFor };
