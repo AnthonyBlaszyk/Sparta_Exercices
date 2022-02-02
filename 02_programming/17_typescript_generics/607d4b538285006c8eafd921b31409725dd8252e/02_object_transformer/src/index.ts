2558696ab6533d3e99617c894ccf67c2a6d17d94
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-02 13:22:08 +0100

    Exercice 02 : typescript_generics clear

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
@@ -1,3 +1,13 @@
+type Product = {
+  productName: string;
+  quantity: number;
+};
+
+function productTransformer(object: Product): [string, string | number][] {
+  return Object.entries(object);
+}
+
+console.log(productTransformer({ productName: "value", quantity: 12 }));
 
 // Leave the line below for tests to work properly.
 export { productTransformer };
