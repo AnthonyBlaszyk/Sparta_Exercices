44b36c63036589fca7996c0bbf52813765f7ed37
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-03 15:28:53 +0100

    Exercice 03 unit_testing_1 clear

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/03_going_further_with_tests/src/index.test.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/03_going_further_with_tests/src/index.test.ts
@@ -7,10 +7,9 @@ beforeEach(() => {
 });
 
 describe("Let the tests on arrays begin !", () => {
-
-  // The following arrays are a short list of "must watch" movies. 
+  // The following arrays are a short list of "must watch" movies.
   // Write a test for each of them, respecting the conditions written in the requirements.
-  // Be sure to always test if you test passes with the right conditions but also if 
+  // Be sure to always test if you test passes with the right conditions but also if
   // it does fail with the wrong conditions. Don't forget `expect.assertions`!
 
   // Here's an example:
@@ -20,45 +19,111 @@ describe("Let the tests on arrays begin !", () => {
     mockArray = ["Into the wild", "Alien the 8th passenger"];
 
     expect(mockArray.length).toBe(2);
-  })
+  });
 
   // Create a test that ensures that "Alien the 8th passenger" is present in mockArray.
   test("Alien the 8th passenger is present in mockArray", () => {
     expect.assertions(1);
 
-    mockArray = ["Into the wild", "Alien the 8th passenger","Ip Man","Tombeau des lucioles", "fight club"];
-  })
+    mockArray = ["Into the wild", "Alien the 8th passenger", "Ip Man", "Tombeau des lucioles", "fight club"];
+
+    expect(mockArray).toContain("Alien the 8th passenger");
+  });
 
   // Create a test that fails if "Indiana Jones 4" is present in mockArray.
-  mockArray = ["Into the wild", "Alien the 8th passenger","Ip Man","Tombeau des lucioles", "fight club"];
+  test("Indiana Jones 4 is not present in mockArray", () => {
+    expect.assertions(1);
+
+    mockArray = ["Into the wild", "Alien the 8th passenger", "Ip Man", "Tombeau des lucioles", "fight club"];
+
+    expect(mockArray).not.toContain("Indiana Jones 4");
+  });
 
   // Create a test fails if the list has less than 5 elements.
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
+  test("mockArray have more than 4 elements", () => {
+    expect.assertions(1);
+
+    mockArray = [
+      "Lord of the rings : the return of the king",
+      "Into the wild",
+      "Alien the 8th passenger",
+      "Tombeau des lucioles",
+      "fight club",
+    ];
+
+    expect(mockArray.length).toBeGreaterThan(4);
+  });
 
   // Create a test that checks if the array only contains String element.
   // Remember that a test is code, you are not limited to using `expect`.
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
+  test("each elements is a string", () => {
+    expect.assertions(1);
+    mockArray = [
+      "Lord of the rings : the return of the king",
+      "Into the wild",
+      "Alien the 8th passenger",
+      "Tombeau des lucioles",
+      "fight club",
+    ];
+
+    expect(mockArray.every((element) => typeof element === "string")).toBeTruthy();
+  });
 
   // Create a test that passes if there's at least one film with "Lord of the rings" inside mockArray.
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
+  test("At least one film Lord of the rings", () => {
+    expect.assertions(1);
+
+    mockArray = [
+      "Lord of the rings : the return of the king",
+      "Into the wild",
+      "Alien the 8th passenger",
+      "Tombeau des lucioles",
+      "fight club",
+    ];
+
+    function checkFilm(filmList: string[]): boolean {
+      for (let i = 0; i < mockArray.length; i++) {
+        if (filmList[i].includes("Lord of the rings")) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    expect(checkFilm(mockArray)).toBe(true);
+  });
 
   //Create a test that checks if the elements in mockArray respect that the strings are the same with the right order as solutionArray
   //eg : mockArray = ["Blue","Blue","Red", "Yellow"] would fail the test
-  mockArray = ["Blue", "Red", "Blue", "Yellow"];
-  const solutionArray = ["Blue", "Red", "Blue", "Yellow"];
+  test("mockArray = solutionArray", () => {
+    expect.assertions(1);
+
+    mockArray = ["Blue", "Red", "Blue", "Yellow"];
+    const solutionArray = ["Blue", "Red", "Blue", "Yellow"];
+
+    expect(mockArray).toEqual(solutionArray);
+  });
 });
 
 describe("Let the tests on objects begin !", () => {
-
   // Create a test that ensures that your 'command' object contains at least `fries: true` and `sauce: "Hannibal"` without checking properties one by one
   // You should do it with only one assertion.
   // Hint: we want our mock object to _match_ another smaller object (look at the documentation!)
-  mockObject = {
-    sauce: "Hannibal",
-    fries: true,
-    kebab: "Double",
-    soda: "Sprite",
-  };
+  test("last test", () => {
+    mockObject = {
+      sauce: "Hannibal",
+      fries: true,
+      kebab: "Double",
+      soda: "Sprite",
+    };
+
+    const correctObject = {
+      fries: true,
+      sauce: "Hannibal",
+    };
+
+    expect(mockObject).toMatchObject(correctObject);
+  });
 });
 
 //At the end, your file should contain 7 test (8 with the given example).
