78b4c044ed48286e731e6e375fb57872c3edec64
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-08 09:51:53 +0100

    Exercice 02 clear

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
@@ -1,1 +1,9 @@
 // Code here
+function sortingMachine(numberArray: number[], bool: boolean): number[] {
+  if (bool === true) {
+    return numberArray.sort((a, b) => a - b);
+  }
+  return numberArray.sort((a, b) => b - a);
+}
+
+export { sortingMachine };
