52b4122457e070b028a4ec33a3aa7c59fcac818f
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-08 09:46:00 +0100

    Exercice 01 clear

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/01_warmup_iterate_on_data/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/01_warmup_iterate_on_data/src/index.ts
@@ -1,1 +1,15 @@
 // Code here
+
+type CustomerType = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+};
+
+function getEmails(email: CustomerType[]): string[] {
+  return email.map((element) => element.email);
+}
+
+export { getEmails };
