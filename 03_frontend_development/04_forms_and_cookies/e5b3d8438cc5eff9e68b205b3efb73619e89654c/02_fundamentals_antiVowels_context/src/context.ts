089964852e28e60e8b71b38838283e18978ea8d2
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-10 10:28:51 +0100

    Exercice 02 clear

--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/context.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/context.ts
@@ -1,7 +1,21 @@
-import { sentenceJoiner } from './sentenceJoiner';
-import { sentenceSplitter } from './sentenceSplitter';
+import { sentenceJoiner } from "./sentenceJoiner";
+import { sentenceSplitter } from "./sentenceSplitter";
 
-
-export const context = () => {
+export const context = (sentence: string): string => {
   // Code your function for context here
+
+  const tmpArray = sentenceSplitter(sentence);
+  const finalArray = [];
+  tmpArray.filter((word) => {
+    if (
+      word.charAt(0).toLowerCase() !== "a" &&
+      word.charAt(0).toLowerCase() !== "e" &&
+      word.charAt(0).toLowerCase() !== "i" &&
+      word.charAt(0).toLowerCase() !== "o" &&
+      word.charAt(0).toLowerCase() !== "u"
+    ) {
+      finalArray.push(word);
+    }
+  });
+  return sentenceJoiner(finalArray);
 };
--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/sentenceJoiner.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/sentenceJoiner.ts
@@ -1,1 +1,5 @@
 // Copy your function `sentenceJoiner` from the first exercise here
+export const sentenceJoiner = (stringArray: string[]): string => {
+  // Code the sentenceJoiner function here
+  return stringArray.join(" ");
+};
