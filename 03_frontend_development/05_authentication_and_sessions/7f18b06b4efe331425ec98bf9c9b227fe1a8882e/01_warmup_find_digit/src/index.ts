5f7e8ef2580074567494c4eeb07682e87a8565c1
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-11 09:46:00 +0100

    Exercice 01 clear

--- 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/01_warmup_find_digit/src/index.ts
+++ 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/01_warmup_find_digit/src/index.ts
@@ -1,1 +1,13 @@
 // Code here
+
+function findDigit(numberArray: number[], numberToFind: number): number[] {
+  const finalArray: number[] = [];
+  numberArray.filter((numb) => {
+    if (numb.toString().includes(numberToFind.toString())) {
+      finalArray.push(numb);
+    }
+  });
+  return finalArray;
+}
+
+export { findDigit };
