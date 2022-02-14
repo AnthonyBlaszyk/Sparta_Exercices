475314527f7a95fc16bbe400b21a12cd6c5540d0
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-14 17:12:32 +0100

    Exercice 04 WIP

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
@@ -1,16 +1,35 @@
 import request from "@fewlines-education/request";
 import "dotenv/config";
 
-function weatherByZipcode(zipcode, countryCode) {
+function weatherByZipcode(zipcode: string, countryCode: string): void {
   // code the function here
+  request(
+    `http://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${countryCode}&units=metric&cnt=16&appid=${process.env.OPENWEATHER_API_KEY}`,
+    (error, html) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const jsonObject = JSON.parse(html);
+        console.log(`Weather for ${jsonObject.city.name}`);
+      }
+    },
+  );
 }
 
-function weatherByLatitudeAndLongitude(latitude, longitude) {
+function weatherByLatitudeAndLongitude(latitude: number, longitude: number): void {
   // code the function here
+  request(
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&cnt=16&appid=${process.env.OPENWEATHER_API_KEY}`,
+    (error, html) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const jsonObject = JSON.parse(html);
+        console.log(`Weather for ${jsonObject.city.name}`);
+      }
+    },
+  );
 }
 
 // leave lines below for tests to work properly
-export {
-  weatherByZipcode,
-  weatherByLatitudeAndLongitude
-}
+export { weatherByZipcode, weatherByLatitudeAndLongitude };
