7383c7e751baa2cc045898c55e6b254b5c5175af
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-14 10:09:41 +0100

    Exercice 01 clear

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/01_warmup_loto_function/src/lottery_draw.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/01_warmup_loto_function/src/lottery_draw.ts
@@ -1,6 +1,9 @@
-type Draw = number[]
+type Draw = number[];
 
 export function lottery_draw(): Draw {
-
+  const lotteryTicket = [];
+  for (let i = 0; i < 6; i++) {
+    lotteryTicket.push(Math.round(Math.random() * 100));
+  }
+  return lotteryTicket;
 }
-
