f0492a9dfd5c4c258cf1cd0eda5e72328194e828
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-14 11:10:22 +0100

    Exercice 03 clear

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
@@ -1,7 +1,15 @@
 import request from "@fewlines-education/request";
 
-function getChuckNorrisJoke() {
+function getChuckNorrisJoke(selectedCategory: string): void {
   // code the function here
+  request("https://api.chucknorris.io/jokes/random?category=" + selectedCategory, (error, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const data = JSON.parse(body);
+      console.log(data);
+    }
+  });
 }
 
 // leave line below for tests to work properly
