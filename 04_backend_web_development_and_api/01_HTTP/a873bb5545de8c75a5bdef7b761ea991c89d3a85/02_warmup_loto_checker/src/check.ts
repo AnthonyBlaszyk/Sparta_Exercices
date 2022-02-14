18b09fd008fe5936197d1bd1164b8d5a43ec6554
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-14 10:31:20 +0100

    Exercice 02 clear

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
@@ -1,6 +1,19 @@
-import { lottery_draw } from "./lottery_draw"
+import { lottery_draw } from "./lottery_draw";
 import { Draw } from "./types";
 
 export function checkIfIWon(ticket: Draw): void {
   // Code the function here
+  const winningTicket = lottery_draw();
+  let winningNumber = 0;
+  for (let i = 0; i < ticket.length; i++) {
+    if (ticket[i] === winningTicket[i]) {
+      winningNumber++;
+    }
+  }
+
+  if (winningNumber === 3) {
+    console.log("You won the lottery!");
+  } else {
+    console.log("You lost...");
+  }
 }
