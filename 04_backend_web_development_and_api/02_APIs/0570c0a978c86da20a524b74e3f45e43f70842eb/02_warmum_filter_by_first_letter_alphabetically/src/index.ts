c6235b8cb6369e0a04142b8c50383eb17a7e3219
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-15 10:08:57 +0100

    Exercice 02 clear

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
@@ -1,1 +1,11 @@
 // code here
+import { Customer } from "../data/customers";
+
+function filterByFirstLetter(clientList: Customer[], firstLetter: string): Customer[] {
+  // Code here
+  return clientList.filter((client) => client.last_name.charAt(0) === firstLetter);
+}
+
+export function filterByFirstLetterOrdered(clientList: Customer[], firstLetter: string): Customer[] {
+  return filterByFirstLetter(clientList, firstLetter).sort((a, b) => a.last_name.localeCompare(b.last_name));
+}
