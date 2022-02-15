59fee54b3c99facea45b6c6716af08e1950187aa
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-15 09:45:29 +0100

    Exercice 01 clear

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
@@ -1,5 +1,6 @@
 import { Customer } from "../data/customers";
 
-export function filterByFirstLetter() {
+export function filterByFirstLetter(clientList: Customer[], firstLetter: string): Customer[] {
   // Code here
+  return clientList.filter((client) => client.last_name.charAt(0) === firstLetter);
 }
