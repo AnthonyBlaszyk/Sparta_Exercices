1fb948d4805427d44d062de7e0385ea9bf26245b
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-21 10:24:11 +0100

    Exercice 03 clear

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
@@ -1,5 +1,9 @@
 function waitFor(time: number): Promise<void> {
-
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log("this will be printed after 2 seconds"));
+    }, time);
+  });
 }
 
 export { waitFor };
