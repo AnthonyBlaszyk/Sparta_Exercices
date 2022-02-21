847ff1e89adaad9661236a7fa271d63c26f044f4
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-21 11:25:36 +0100

    Exercice 05 clear

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
@@ -1,7 +1,13 @@
 import { getRandomFace } from "./random-face";
 
-function headOrTail() {
-
+function headOrTail(choice: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (choice === getRandomFace()) {
+      resolve("Well done.");
+    } else {
+      reject("Nope.");
+    }
+  });
 }
 
 export { headOrTail };
