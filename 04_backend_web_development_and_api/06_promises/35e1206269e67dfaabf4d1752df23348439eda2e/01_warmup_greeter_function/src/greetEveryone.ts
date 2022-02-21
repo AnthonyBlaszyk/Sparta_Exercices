54a5a0cb41c8bf8780cd11129de136b95c473cb1
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-21 09:59:30 +0100

    Exercice 01 clear

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/01_warmup_greeter_function/src/greetEveryone.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/01_warmup_greeter_function/src/greetEveryone.ts
@@ -1,5 +1,8 @@
 type ParamRest = string[];
 
-export function greetEveryone(): void {
+export function greetEveryone(...greet: string[]): void {
   // Code your function here
+  greet.forEach((person) => {
+    console.log(`Welcome to ${person}`);
+  });
 }
