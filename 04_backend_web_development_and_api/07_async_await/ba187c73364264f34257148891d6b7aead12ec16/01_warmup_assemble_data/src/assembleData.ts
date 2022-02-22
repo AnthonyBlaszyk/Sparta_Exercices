2438c5f1e629066642de00ddcad73b9594371ee0
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-22 10:03:17 +0100

    Exercice 01 clear

--- 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/01_warmup_assemble_data/src/assembleData.ts
+++ 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/01_warmup_assemble_data/src/assembleData.ts
@@ -5,6 +5,11 @@ type Profile = {
   haveTraveled: boolean;
 };
 
-export function assembleData() {
+export function assembleData(names: string[], ages: number[], countries: string[], haveTravaled: boolean[]): Profile[] {
   // Code here
+  const assembleData = [];
+  for (let i = 0; i < names.length; i++) {
+    assembleData.push({ name: names[i], age: ages[i], country: countries[i], haveTraveled: haveTravaled[i] });
+  }
+  return assembleData;
 }
