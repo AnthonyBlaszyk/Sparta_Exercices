0cfc4c9f6a4d260b0f53b116a45f13ca8bb62d6b
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-23 11:27:36 +0100

    Exercice 01 clear

--- 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/01_warmup_parse_name/src/parse-name.ts
+++ 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/01_warmup_parse_name/src/parse-name.ts
@@ -1,7 +1,9 @@
 type Person = {
   firstName: string;
   lastName: string;
-}
+};
 
 export function parseName(name: string): Person {
+  const nameSplit = name.split(" ");
+  return { firstName: nameSplit[0], lastName: nameSplit[1] };
 }
