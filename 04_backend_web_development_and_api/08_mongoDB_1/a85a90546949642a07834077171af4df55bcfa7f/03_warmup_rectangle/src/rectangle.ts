aabf5d36ad1abbdb9522b6a484839b9238a8c79f
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-23 11:37:22 +0100

    Exercice 03 clear

--- 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
+++ 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
@@ -1,2 +1,10 @@
 export function rectangle(width: number, height: number): string {
+  let star: string = "";
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      star += "*";
+    }
+    star += "\n";
+  }
+  return star;
 }
