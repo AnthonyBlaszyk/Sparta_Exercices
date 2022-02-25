cf996876776b9694bcdebd7e3c1ae1d26eb361cb
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-25 10:21:37 +0100

    Exercice 03 clear

--- 04_backend_web_development_and_api/10_mongoDB_3/360813d55cef02adff3e4b5cb941209840555dd2/03_warmup_display_rectangles/src/rectangle.ts
+++ 04_backend_web_development_and_api/10_mongoDB_3/360813d55cef02adff3e4b5cb941209840555dd2/03_warmup_display_rectangles/src/rectangle.ts
@@ -2,5 +2,6 @@ import { line } from "./line";
 import { column } from "./column";
 
 export function rectangle(width: number, height: number): string {
-
+  const starLine = line(width);
+  return column(height, starLine);
 }
