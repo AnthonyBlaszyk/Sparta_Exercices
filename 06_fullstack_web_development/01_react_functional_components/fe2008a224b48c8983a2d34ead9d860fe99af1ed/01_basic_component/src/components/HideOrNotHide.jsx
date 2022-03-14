48e88b0904cd98f3b9bd333ae68dd8805e36b3fe
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-03-14 10:42:46 +0100

    Exercice 01 clear

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/01_basic_component/src/components/HideOrNotHide.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/01_basic_component/src/components/HideOrNotHide.jsx
@@ -2,4 +2,20 @@ import React from "react";
 
 export const HideOrNotHide = () => {
   // Code here
+  let [showtext, setShowtext] = React.useState(true);
+
+  if (showtext) {
+    return (
+      <div>
+        <button onClick={() => setShowtext((showtext = false))}>Paragraph visible</button>
+        <p>Some Text</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button onClick={() => setShowtext((showtext = true))}>Paragraph hidden</button>
+      </div>
+    );
+  }
 };
