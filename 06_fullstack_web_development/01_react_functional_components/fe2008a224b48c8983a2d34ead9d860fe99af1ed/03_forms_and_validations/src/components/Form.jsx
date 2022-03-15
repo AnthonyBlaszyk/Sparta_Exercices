0c2885bde5039e298524f830ce310256ae7408a7
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-03-15 14:42:58 +0100

    Exercice 03 clear

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [isValide, setisValide] = React.useState(true);
+
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log("You clicked submit.");
+  }
+
+  function onChange(event) {
+    const value = event.target.value;
+    value.includes(" ") ? setisValide(false) : setisValide(true);
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <label>First Name</label>
+        {isValide ? (
+          <div>
+            <input onChange={onChange} className="form-control" />
+          </div>
+        ) : (
+          <div>
+            <input onChange={onChange} className="form-control is-invalid" />
+            <div className="invalid-feedback">No spaces allowed</div>
+          </div>
+        )}
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Form;
